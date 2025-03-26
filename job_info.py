b15356ea82c17d62a9f7f590fd6c6d346f31abab
Lila Rungcharoenporn <yrung16@gmail.com>
2025-03-25 17:30:04 -0700

    get company and location for all 15 pages

--- job_info.py
+++ job_info.py
@@ -1,24 +1,14 @@
 from bs4 import BeautifulSoup
  
  # Read the HTML file
-with open("6.html", "r", encoding="utf-8") as file:
+with open("9.html", "r", encoding="utf-8") as file:
   html_content = file.read()
  
 # Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(html_content, "html.parser")
- 
-#  company = soup.find('div', attrs={'data-testid': 'inlineHeader-companyName'}).get_text()
-#  location = soup.find('div', attrs={'data-testid': 'inlineHeader-companyLocation'}).get_text()
-#  salaryAndJobType = soup.find('div', id='salaryInfoAndJobType').get_text()
-#  jobDescription = soup.find('div', id='jobDescriptionText').get_text()
- 
-#  print(company)
-#  print(location)
-#  print(salaryAndJobType)
-#  print(jobDescription)
 
-company = soup.find('span', attrs={'class': 'jobsearch-JobInfoHeader-companyNameSimple'}).get_text()
-location = soup.find('div', attrs={'data-testid': 'jobsearch-JobInfoHeader-companyLocation'}).get_text()
+company = soup.find(attrs={'class': lambda x: x and 'companyName' in x}).get_text()
+location = soup.find(attrs={'data-testid': lambda x: x and 'location' in x or x and 'Location' in x}).get_text()
 salary = soup.find('li', attrs={'data-testid': 'list-item'}).get_text()
 jobDescription = soup.find('div', id='jobDescriptionText').get_text()
 
